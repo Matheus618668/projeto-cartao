f5efff82fcaf8edec0a86e963cf1ee19edc7043b
Matheus618668 <matheus.neves@moonventures.com.br>
2025-04-10 18:48:25 -0300

    app.py

--- app.py
+++ app.py
@@ -229,9 +229,9 @@ if menu == "Inserir Compra":
                 st.success("✅ Compra registrada com sucesso!")
                 st.session_state.form_submitted = True
 
-        if st.session_state.get("form_submitted"):
-           if st.button("🆕 Nova Compra"):
-        for key in st.session_state.keys():
+       if st.session_state.get("form_submitted"):
+    if st.button("🆕 Nova Compra"):
+        for key in list(st.session_state.keys()):
             del st.session_state[key]
         st.rerun()
 
